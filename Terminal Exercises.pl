609ef71742a780ec2e6b5705c6d0d07e904531dc
Kevin-Mendez48 <126734523+Kevin-Mendez48@users.noreply.github.com>
2024-04-14 10:13:22 -0400

    Add files via upload

---
+++ Terminal Exercises.pl
@@ -0,0 +1,35 @@
+Terminal Exercises
+Part 1:
+
+1. To make a directory called 'first', we use the command 'mk dir' followed by the name you would like to use. In this case 'first'
+2.  To change directory we use the command 'cd' and we will use 'first' as the chosen folder
+3. To create a file we use the command 'touch' and name is 'person.txt'
+4. To change the name of a file, we use the command 'mv' and the name of the first, plus the name it will be changed to. which would be 'person.txt another.txt'
+5.To make a copy of a file we use the comand 'cp' and the file name 'another.txt', followed by the name to give the copy. In this case 'copy.txt'
+6. To remove a file we use 'rm', and the name of the file 'copy.txt'
+7. To copy a directory and name is something else, we use 'cp -r' followed by the name of the file and the copy. The -r makes a copy of all the contents, including subdirectories and all of their content.
+8. To delete a copy, we use the same 'rm' command as proir, just with the '-r' flag to delete all of the subdirectories and all the content.
+
+1. mkdir first
+2. cd first
+3. touch person.txt
+4. mv person.txt another.txt
+5. cp another.txt copy.txt
+6. rm copy.txt
+7. cp -r first second
+8. rm -r second
+
+Part 2:
+
+1. The 'man' command would display an interfact to the system reference manuals. To exit one would press 'q'
+2. The '-a' says 'do not ignore entries starting with', which means to not ignore any entries. it will dipslay everything starting with a dot.
+The '-l' displays everything in a long listing format.
+3. One can jump between words in the terminal by holding down alt with an arrow key.
+4. The end of the terminal is reached by pressing Ctrl with 'e'
+5. Pressing Ctrl with 'a' will move the cursor to the beginning of the line.
+6. Pressuing Ctrl with 'w' will delete an entire word.
+7. The terminal is where you use text to interact with a computer. The shell is where one enters commands which is interpreted by the computer. The terminal window is how one interacts with the shell.
+8. An absolute path is the entirety of the file/directory up until the root.
+9. A relative path will have a few dots in place of 'parent' directories, only showing the current location
+10. A flag is used to modify a command. for example, earlier we used -r for the 'rm' command, which makes it delete all subdirectories not just the current.
+11. The 'r' that has been used before stands for 'recursive'. The '-f' flag will suppress most error messages.
